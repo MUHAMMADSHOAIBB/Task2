8a9a442c883ffa81d0dc39f1a3474bac9073aa1d
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-01 10:43:26 +0500

    Add files via upload
    Task 2.1
    Former creeps selected 15 candidates as their permanent employees,
    Ali
    Asad
    Farah
    Shameer
    Subhan
    Salar
    Hafsa
    Sana
    Hoora
    Ahad
    Saif
    Maryam
    Irfan
    Furqan
    Mehroz
    they are straightforward , and very choosy. No one can argue about their selection and the permanent
    candidates feel secure once they are having this position... You task is to create a collection for "
    Formercreep's Permanent employees"
    Task 2.2
    Gluty is a new brand that serves limka all over in Pakistan. They are serving various flavour of limka. Every
    month they introduced a "new" flavor ., These are some of its flavors
    Orange, pineapple, grapes, tutyfruty, pepsi, pakola , reditrish .
    Consider yourself as a product manager of Gluty, your task is to update their menu on monthly basis. In which
    collection you serve Gluty',s product.
    Task 2.3
    HooraPharma is a leading diagnostic medical tools provider company that serves as a third party to sell the
    products of "Seimens". Hoorapharma signed an agreement with seimens that they only sell their products ,
    they are not allowed to sell other company products.
    Currently these products are in their stock
    Ventilator, CTscan , Defibrillator, ECG machine, Patient monitor, Syringes, MRI.
    An employee of "Nihon kohden" came to the manager of Hoorapharma and asked him to sell their Patient
    monitor and Defibrillator.
    What do you think Manager of Hoorapharma will accept his offer?? Make a collection of their previous stock
    and Guess what will be the reply of Hoorapharma's manager.

---
+++ Task 2.py
@@ -0,0 +1,15 @@
+#Assignment number 2
+print('================Task 2.1===========')
+employes=('Ali','Asad','farah','shahmeer','subhan','salar','hafsa','saan','hoora','ahad','saif','maryam','irfan','furqan','mehroz')
+print(type(employes))
+print(employes)
+print('=====================Task 2.2=======================')
+#Medical products
+products={'ventilator','CTscan','defibrilator','ECG machine','PAtient monitor','syringes','MRI'}
+print(type(products))
+print(products)
+print('====================Task 2.3==================')
+#Gluty product
+Flavours={'orange','pineapple','grapes','tutyfruity','pepsi','pakola','reditrish'}
+print(type(Flavours))
+print(Flavours)
